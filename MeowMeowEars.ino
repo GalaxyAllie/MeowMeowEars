0ade65bf4c83fe48b21f538ba95252f972bac45f
Allie <galaxymergirl@gmail.com>
2020-02-10 18:26:09 +0000

    Added twinkle mode + more comments

--- MeowMeowEars.ino
+++ MeowMeowEars.ino
@@ -1,27 +1,28 @@
 // Set delay for rainbow generation
 int delayTime = 20;
 
-// Create variables used later
-int colourSel = 0;
-int pixelDisp = 0;
-int incomingInt = 0;
-
 // Include Adafruit libraries
 #include <Adafruit_NeoPixel.h>
 
 // Configure light strips
 #define flowerPin 0
 #define flowerCount 10
- 
+
 Adafruit_NeoPixel flowers = Adafruit_NeoPixel(flowerCount, flowerPin, NEO_GRB + NEO_KHZ800);
 
 void setup() {
     flowers.begin();
     flowers.show();
-    flowers.setBrightness(15);
+// - USE CAUTION ABOVE BRIGHTNESS 63!!! Linear regulator can get *hot* and battery life will be greatly reduced for little visible difference!
+    flowers.setBrightness(63);
 }
 
 void loop() {
+// 
+// - Main Loop - uncomment section you want to run - 
+// 
+// - Rainbow Life -
+// 
     for(long firstFlowerColour = 5*65536; firstFlowerColour > 0; firstFlowerColour -= 256) {
         for(int pixelSel = 0; pixelSel < flowerCount; pixelSel++){
             int colourSel = firstFlowerColour + (pixelSel * 65536L / flowerCount);
@@ -31,4 +32,18 @@ void loop() {
         flowers.show();
         delay(delayTime);
     }
+// 
+// - Twinkle Twinkle, Little Star -
+// 
+    // for(int pixelSel = 0; pixelSel < flowerCount; pixelSel++){
+    //     int catSelect = random(0, 10);
+    //     if(catSelect == 3){
+    //         flowers.setPixelColor(pixelSel, 0x00FFFFFF);
+    //     }
+    //     else{
+    //         flowers.setPixelColor(pixelSel, 0x00000000);
+    //     }
+    // }
+    // flowers.show();
+    // delay(random(30,100));
 }
